ee660873ef0d57118e0633b1ed04a89f6dc0516e
Quan Le <quanle@Quans-MacBook-Pro-2.local>
2019-03-29 16:52:27 -0700

    pulled data from database

--- public/api/getproducts.php
+++ public/api/getproducts.php
@@ -1,10 +1,58 @@
 <?php
 
-// include
-// include_once
-// require
-require_once('mysqlconnect.php')
-// readfile('./data/getproducts.json');
-// print('test2')
+require_once('mysqlconnect.php');
 
+$query = 'SELECT p.`id`, p.`name`, p.`price`,
+        i.`url` AS `images`
+    FROM `products` AS p
+    JOIN `images` AS i
+        ON p.`id` = i.`products_id`
+    ORDER BY p.`id` DESC'
+;
+
+// procedural
+$result = mysqli_query($conn, $query);
+
+$data = [];
+
+while($row = mysqli_fetch_assoc($result)) {
+    $currentID = $row['id'];
+
+    if ( isset( $data[$currentID] ) ) {
+        $data[$row['id']]['images'][] = $row['images'];
+
+        // $currentID = $row['id'];
+        // $image = $row['images'];
+        $data[$currentID]['images'][] = $image; // new push way
+        // array_push($data[$currentID]['images'], $image); // old push way
+        // $data[$currentID]['images'][count($date[$currentID]['images'])] = $image;
+        // null;
+    } else {
+        $image = $row['images'];
+        unset($row['images']);
+        $row['images'][] = $image;
+
+        $row['price'] = intval($row['price']);
+        $row['price'] = (int)$row['price'];
+        
+        $data[$currentID] = $row;
+    };
+}
+
+$pureData = [];
+
+foreach($data as $value) {
+    $pureData[] = $value;
+}
+
+exit();
+
+$output = [
+    'success' => true,
+    'products' => $pureData
+];
+
+$json_output = json_encode($output);
+
+print($json_output);
 ?>
--- public/api/msqlconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// procedural style
-$conn = mysqli_connect('localhost', 'root', 'root', '219_wickedsales', 8888);
-
-// oop
-$db = new mysqli('localhost', 'root', 'root', '219_wickedsales', 8888);
-
-?>
