c949fd4f4576fc7e9f65ae26851818199d0999b5
Daniel Paschal <daniel.paschal@learningfuze.com>
2019-03-29 16:51:36 -0700

    added basic product list endpoint wrote query for product list added join for images pushed images into array fixed price to be number converted data to json

--- public/api/getproducts.php
+++ public/api/getproducts.php
@@ -2,4 +2,63 @@
 
 require_once('mysqlconnect.php');
 
+$query = "SELECT p.`id`, p.`name`, p.`price`,
+		i.`url` AS `images`
+	FROM `products` AS p
+	JOIN `images` AS i
+		ON p.`id` = i.`products_id`
+	ORDER BY p.`id`
+";
+
+
+
+
+/*procedural*/
+$result = mysqli_query($conn, $query);
+
+$data = [];
+
+while($row = mysqli_fetch_assoc($result)){
+	$currentID = $row['id'];
+	$currentID = intval($currentID);
+	$image = $row['images'];
+	if( isset( $data[$currentID] ) ){
+		//$data[ $row['id'] ]['images'][] = $row['images'];
+		$data[$currentID]['images'][] = $image; //new push way
+		//array_push($data[$currentID]['images'], $image); //old push way
+		//$data[$currentID]['images'][ count($data[$currentID]['images'])] = $image //this is just mean
+	} else {
+		unset($row['images']);
+		$row['images'] = [];
+		//array_push($row['images'], $image);
+		$row['images'][] = $image;
+		//the way I would write it
+		//$data[$row['id']]['images'] = [$row['images']];
+
+		$row['price'] = intval($row['price']);
+		//$row['price'] = (int)$row['price']; using casting
+
+		$data[$currentID] = $row;
+
+	}
+}
+
+$pureData = [];
+foreach($data as $value){
+	$pureData[] = $value;
+}
+
+$output = [
+	'success'=>true,
+	'products'=> $pureData
+];
+
+$json_output = json_encode( $output );
+
+print( $json_output );
+
 ?>
+
+
+
+
