d380463c7b9c0dd1926f58f0b02fc5586eab4e43
Scott Bowler <scott.bowler@learningfuze.com>
2019-04-02 16:56:46 -0700

    Completed initial routing setup

---
+++ src/components/home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default props => (
+    <div className="home">
+        <h1 className="center purple-text text-darken-2">🧛🏼‍ Wicked Sales‍ 🧛🏼‍</h1>
+    </div>
+)
---
+++ src/components/products/product_list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+    render(){
+        return (
+            <div className="product-list">
+                <h1 className="center">Wicked Product List</h1>
+            </div>
+        );
+    }
+}
+
+export default ProductList;
