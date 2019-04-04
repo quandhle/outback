abbaf3ee62f2ee74f64f7969176eb1e4307e8b43
Quan Le <quanle@Quans-MacBook-Pro-2.local>
2019-04-04 13:39:22 -0700

    get product details

--- src/components/app.js
+++ src/components/app.js
@@ -5,9 +5,11 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import ProductRoutes from './products';
 import Home from './home';
+import Nav from './nav/index';
 
 const App = () => (
     <div>
+        <Nav/>
         <Route exact path="/" component={Home}/>
         <Route path="/products" component={ProductRoutes}/>
     </div>
