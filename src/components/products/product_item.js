71b9ab714e0454cb099a015ae3a328d9e6dd30e5
Quan Le <quanle@Quans-MacBook-Pro-2.local>
2019-04-05 10:29:53 -0700

    updated product_items

--- src/components/products/product_item.js
+++ src/components/products/product_item.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {formatMoney} from '../../helpers';
 
-console.log('Format monies: ', formatMoney(1113212));
-
 export default ({name, price, images: [productImg = '']}) => {
     return (
         <li className="collection-item avatar">
