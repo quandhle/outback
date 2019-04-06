6385d61bece74447f6456bacbafb3c5b50a8b64c
Quan Le <quanle@Quans-MacBook-Pro-2.local>
2019-04-05 23:09:10 -0700

    added getitems.json, updated handle cors on config.php

---
+++ public/api/addcartitem.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once('functions.php');
+set_exception_handler('handleError');
+require_once('config.php');
+require_once('mysqlconnect.php');
+
+$product_id = 1;
+$product_quantity = 1;
+// $cart_id = 1;
+$user_id = 1;
+
+$query = "SELECT `price` FROM `products` WHERE `id` = $product_id";
+
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    throw new Exception(mysqli_error($conn));
+};
+
+if (mysqli_num_rows($result) === 0) {
+    throw new Exception("No products match product ID $product_id.");
+};
+
+$product_data = mysqli_fetch_assoc($result);
+
+$product_price = (int)$product_data['price'];
+
+$product_total = $product_price * $product_quantity;
+
+if (empty($card_id)) {
+    $cart_create_query = "INSERT INTO `carts` SET
+        `item_count` = $product_quantity,
+        `total_price` = $product_total,
+        `created` = NOW(),
+        `users_id` = $user_id,
+        `changed` = NOW()";
+
+        $cart_result = mysqli_query($conn, $$cart_create_query);
+
+        if (!$cart_result) {
+            throw new Exception(mysqli_error($conn));
+        };
+
+        if (mysqli_affected_rows($conn)) {
+            throw new Exception('Data was not added to cart table.');
+        }
+
+        $cards_id = mysqli_insert_id($conn);
+};
+
+$cart_item_query = "INSERT INTO `cart_items` SET
+    `product_id` = $product_id,
+    `quantity` = $product_quantity,
+    `cards_id` = $card_id";
+
+$cart_item_result = mysli_query($conn, $cart_item_query);
+
+if (!$result) {
+    throw new Exception(mysli_error($query));
+};
+
+if (mysqli_affected_rows($conn) === 0) {
+    throw new Exception('Failed to insert into cart items.');
+};
+
+$output = [
+    "success" => true,
+    "cartCount" => $product_quantity,
+    "cartTotal" => $product_total
+];
+
+print(json_encode($output));
+
+
+$cart_query = "SELECT `id` FROM `carts` WHERE `id` = $cart_id";
+
+$cart_result = mysqli_query($conn, $query);
+
+if (!$result) {
+    throw new Exception(mysqli_error($conn));
+};
+
+if (mysqli_num_rows($result) === 0) {
+    throw new Exception("No carts match cart ID $cart_id");
+};
+
+?>
