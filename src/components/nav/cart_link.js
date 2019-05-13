42f4850feb787a739ddfa7b136102b2dfd3d1832
Quan Le <quandhle@gmail.com>
2019-05-13 10:13:16 -0700

    adjusted nav bar height added products added company info to database and now displays it

--- src/components/nav/cart_link.js
+++ src/components/nav/cart_link.js
@@ -4,8 +4,7 @@ import {Link} from 'react-router-dom';
 export default props => {
     return (
         <Link to="/cart" className="cart-link">
-            <i className="material-icons cart-icon">shopping_cart</i>
-            <span className="side-nav-cart">View Cart</span>
+            <i className="material-icons cart-icon">shopping_cart</i><span className="side-nav-cart">View Cart</span>
             <span className="cart-items">{props.items}</span>
         </Link>
     )
