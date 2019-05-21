d1abc308cefaefae2ced7d6d648d449791cff15b
Quan Le <quandhle@gmail.com>
2019-05-21 15:27:06 -0700

    fixed error

--- public/api/login.php
+++ public/api/login.php
@@ -34,7 +34,7 @@ $query_result = mysqli_stmt_execute($statement);
 $result = mysqli_stmt_get_result($statement);
 
 if (!$result) {
-    throw new Exception(mysqli_error($result));
+    throw new Exception(mysqli_error($conn));
 };
 
 if (mysqli_num_rows($result) !== 1) {
